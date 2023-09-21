739a5ccc60964dbaba7c604eacd81e6c3e7adbb5
feketegyz <73114094+feketegyz@users.noreply.github.com>
2023-09-21 15:26:14 +0200

    pxt.json, main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 let kezdy = randint(0, 4)
-let kezx = randint(0, 2)
-led.plot(kezx, kezdy)
+let kezdx = randint(0, 2)
+led.plot(kezdx, kezdy)
 for (let index = 0; index < 2; index++) {
-    kezx += 1
-    led.plot(kezx + 0, kezdy)
+    kezdx += 1
+    led.plot(kezdx + 0, kezdy)
 }
 basic.forever(function () {
 	
