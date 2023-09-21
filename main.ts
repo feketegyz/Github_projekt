ee5c779f737d2d62704e07a34493ff9d8d4b56af
feketegyz <73114094+feketegyz@users.noreply.github.com>
2023-09-21 15:02:01 +0200

    hajó elhelyezése

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-basic.showIcon(IconNames.StickFigure)
+let kezdy = randint(0, 4)
+let kezx = randint(0, 2)
+led.plot(kezx, kezdy)
+for (let index = 0; index < 2; index++) {
+    kezx += 1
+    led.plot(kezx + 0, kezdy)
+}
 basic.forever(function () {
 	
 })
