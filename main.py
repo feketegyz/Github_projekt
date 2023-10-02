ddc2a09625181dafe0897c49114dfbd7186416b9
feketegyz <73114094+feketegyz@users.noreply.github.com>
2023-10-02 15:23:42 +0200

    main.blocks, main.ts, main.py frissítése

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+kezdy = randint(0, 4)
+kezdx = randint(0, 2)
+led.plot(kezdx, kezdy)
+for index in range(2):
+    kezdx += 1
+    led.plot(kezdx + 0, kezdy)
+
 def on_forever():
     pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 let kezdy = randint(0, 4)
 let kezdx = randint(0, 2)
-led.plot(kezdx, kezdy)
-for (let index = 0; index < 2; index++) {
+let lista = [
+kezdy,
+kezdx,
+kezdx + 1,
+kezdx + 2
+]
+for (let index = 0; index < 3; index++) {
+    led.plot(kezdx, kezdy)
     kezdx += 1
-    led.plot(kezdx + 0, kezdy)
 }
 basic.forever(function () {
 	
